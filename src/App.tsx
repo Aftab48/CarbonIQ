35f3cc0da9d41259fd06df1538e711494a4d7401
Aftab Alam <mdalam4884@gmail.com>
2024-10-21 02:05:43 +0530

    setup 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="flex items-center justify-center w-full  h-screen">
+    <div className="flex items-center justify-center w-full h-screen">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
